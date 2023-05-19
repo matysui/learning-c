b2c74848426be1b6c02856945cba9ed3c752ce10
Renan Gabriel <126295397+renan8e@users.noreply.github.com>
2023-05-19 15:46:17 -0300

    sybolic constants

--- index.c
+++ index.c
@@ -1,23 +1,16 @@
 #include <stdio.h>
 
-/* print Fahrenheit-Celsius table
-for fahr = 0, 20,..., 300 */
+// print Fahrenheit-Celsius table
+
+#define		LOWER  0 /* lower limit of temperature table*/
+#define		UPPER  300 /* upper limite */	
+#define		STEP  20 /*step size*/
 
 int main()
 {
-	float fahr, celsius;
-	int lower, upper, step;
-
-	lower = 0; /* lower limit of temperature table*/
-	upper = 300; /* upper limite */	
-	step = 20; /*step size*/
-
-	fahr = lower;
-	while (fahr<=upper){
-		celsius = (5.0/9.0) * (fahr-32.0);
-		printf("%3.0f\t%6.1f\n",fahr,celsius);
-		fahr +=step;
-	}
+	int fahr;
+	for (fahr = LOWER;fahr <=UPPER; fahr = fahr + STEP)
+		printf("%3d %6.1f \n", fahr, (5.0/9.0) * (fahr-32) );
 
 }
 
