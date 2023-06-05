f18a4aa43603bab30aed1d85e909acc460396453
Renan Gabriel <126295397+renan8e@users.noreply.github.com>
2023-06-05 15:27:50 -0300

    count lines words and characters putchar getchar

--- index.c
+++ index.c
@@ -1,16 +1,29 @@
 #include <stdio.h>
 
-//count lines in input
+
+#define IN 1 /* inside a word */
+#define OUT 0 /* outside a word/space,tab etc  */
+//count lines,words, and characters in input
 int main()
 {
-	int c ,nl;
-	nl = 0 ;
-
-	while ((c = getchar()) != EOF){
-		//everytime you press enter/'\n' this will happen
-		if (c == '\n'){
+	int c ,nl, nw, nc,state;
+	state = OUT ;
+	nl = nw = nc = 0;
+	while((c = getchar()) != EOF) {
+// everytime this loop pass through a character nc will count a letter
+		++nc;
+		// detect a newline and increment
+		if (c == '\n')
 			++nl;
+		// detect a spacebar, tab or newline and change the state
+		if (c == ' ' || c == '\n' || c == '\t'){
+			state = OUT ;
+		}
+		//this will revert the stage to "inside a word" and increment nw-var
+		else if (state == OUT) {
+			state = IN ;
+			++nw;
 		}
 	}
-	printf("%d\n",nl);
+	printf("\n%d %d %d\n",nl,nw,nc);
 }
