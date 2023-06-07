672a09528dd3e96444aabc08e385c433e71a1ee0
Renan Gabriel <126295397+renan8e@users.noreply.github.com>
2023-06-07 14:25:20 -0300

    Vertical Histogram

--- index.c
+++ index.c
@@ -1,20 +1,53 @@
 #include <stdio.h>
 
-
+#define MAXWL 20 // maximun length of a word
+#define MAXNO 25 // MAXIMUN NO OF WORD IN A SENTENCE
 int main()
 {
-	int c ;
+	int word[MAXNO];
+	int c, i, j, nc, nw;
+
+	for (i = 0;i < MAXNO ;++i )
+	{
+		word[i] = 0;
+	}
+
+	nw = nc = 0;
 
 	while((c = getchar()) != EOF)
 	{
+		//help to count the number of characteres
+		++nc;
+		// preparing for new word/ go to next element of the array/word
 		if(c == ' '|| c == '\n' || c == '\t')
 		{
-			putchar('\n');
+			// -1 for excluding the space in the word length
+			word[nw] = nc - 1 ;
+
+			// resetting the word-lenght for next word
+			++nw ;
+
+			nc = 0;
+
 		}
-		// every time c is a characters-letter||number this will print a pipe
-		else
+	}
+// HISTOGRAM  ' ''*'' ''*'...
+	for (i = MAXWL;i >= 1 ; --i ) // EXC 20v
+	{
+		//make the '''*''*'... lines
+		for(j = 0; j <= nw; ++j )
 		{
-			putchar('|');
+			// decide between ' ' or *
+			// (i <= number-length)
+			if (i <= word[j])
+			{
+				putchar('*');
+			}
+			else
+			{
+				putchar(' ');
+			}
 		}
+		putchar('\n');
 	}
 }
