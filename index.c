2c01caaa562f6794b68b11349bdb25ecadffb290
Renan Gabriel <126295397+renan8e@users.noreply.github.com>
2023-06-08 15:48:30 -0300

    power function

--- index.c
+++ index.c
@@ -1,53 +1,22 @@
 #include <stdio.h>
 
-#define MAXWL 20 // maximun length of a word
-#define MAXNO 25 // MAXIMUN NO OF WORD IN A SENTENCE
-int main()
-{
-	int word[MAXNO];
-	int c, i, j, nc, nw;
+int power(int m,int n);
 
-	for (i = 0;i < MAXNO ;++i )
-	{
-		word[i] = 0;
+// test power function
+int main(){
+	int i ;
+
+	for (i = 0; i < 10; ++i ) {
+		printf("%d %d %d\n",i,power(2,i),power(-3,i)) ;
 	}
+	return 0;
+}
+// power : raises base to  n-th power ; n >= 0
+int power(int base,int n) {
+	int i ,p ;
+	p = 1 ;
 
-	nw = nc = 0;
-
-	while((c = getchar()) != EOF)
-	{
-		//help to count the number of characteres
-		++nc;
-		// preparing for new word/ go to next element of the array/word
-		if(c == ' '|| c == '\n' || c == '\t')
-		{
-			// -1 for excluding the space in the word length
-			word[nw] = nc - 1 ;
-
-			// resetting the word-lenght for next word
-			++nw ;
-
-			nc = 0;
-
-		}
-	}
-// HISTOGRAM  ' ''*'' ''*'...
-	for (i = MAXWL;i >= 1 ; --i ) // EXC 20v
-	{
-		//make the '''*''*'... lines
-		for(j = 0; j <= nw; ++j )
-		{
-			// decide between ' ' or *
-			// (i <= number-length)
-			if (i <= word[j])
-			{
-				putchar('*');
-			}
-			else
-			{
-				putchar(' ');
-			}
-		}
-		putchar('\n');
-	}
+	for (i = 1;i <= n; ++i)
+		p = p * base ;
+	return p;
 }
