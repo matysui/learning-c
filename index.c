f2da4d326010381211137f693d33336744d005b2
Renan Gabriel <126295397+renan8e@users.noreply.github.com>
2023-07-19 17:55:43 -0300

    squeeze concept

--- index.c
+++ index.c
@@ -1,55 +1,20 @@
 #include <stdio.h>
-#define MAXLINE 1000 // maximun input line size
 
-int max; /*maximum length seen so far */
-char line[MAXLINE] ; /*current input line*/
-char longest[MAXLINE]; /*longest line saved here*/
-
-
-int getLine(void);
-void copy(void);
+void tira(char s[],int c);
 
 int main(){
-	int len; // current line length
-	extern int max ;
-	extern char longest[];
-	max = 0;
-	while((len = getLine()) > 0)
-	{
-		// comparação
-		if(len > max)
-		{
-			max = len;
-			copy();
-		}
-	}
-	if (max > 0) //there was a line
-				 printf("%s",longest);
+	char palur[] = "sakura";
+	char mal = 'a';
+	tira(palur,mal);
 
-	return 0;
+	printf("%s\n",palur);
 }
 
-int getLine(void){
-	int c,i;
-	extern char line[];
-	for (i = 0;i < MAXLINE- 1 && (c =getchar()) != EOF && c != '\n';++i){
-		line[i] = c ;
-	}
-// xD now i see
-	if (c == '\n'){
-		line[i] = c;
-		++i;
-	}
-	line[i] = '\0';
-	return i ;
-}
+void tira(char s[],int c ){
+	int j,i;
+	for (i= j = 0;s[i] != '\0'; i++)
+		if(s[i] != c)
+			s[j++] = s[i];
+	s[j] = '\0';
 
-void copy(void){
-	int i ;
-	extern char line[],longest[];
-
-	i = 0;
-	while((longest[i] = line[i]) != '\0'){
-		++i;
-	}
 }
