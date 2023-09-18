d7500a2e227a5de96625f9d3abea47d8b9c64190
Renan8e <monodrone100@gmail.com>
2023-09-18 02:48:45 -0300

    understanding squeeze() function

--- index.c
+++ index.c
@@ -2,19 +2,29 @@
 
 void tira(char s[],int c);
 
-int main(){
+int main()
+{
 	char palur[] = "sakura";
 	char mal = 'a';
+
 	tira(palur,mal);
 
 	printf("%s\n",palur);
 }
 
-void tira(char s[],int c ){
+void tira(char s[],int c )
+{
 	int j,i;
-	for (i= j = 0;s[i] != '\0'; i++)
-		if(s[i] != c)
-			s[j++] = s[i];
+
+	for (i= j = 0;s[i] != '\0'; i++)// will run since s[i] is not
+	{
+		if(s[i] != c)// will execute if s[i] is different from c('a')
+			{
+
+			s[j++] = s[i];//each time a non-c occurs, it is copied into the j position.then the string is modified
+
+			}
+	}
 	s[j] = '\0';
 
 }
