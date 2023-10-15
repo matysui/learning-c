b8430e6585d487268d5732413a9c81f099e20951
Renan Gabriel <126295397+renan8e@users.noreply.github.com>
2023-10-15 15:45:02 -0300

    binarysearch exc3-1

--- index.c
+++ index.c
@@ -4,24 +4,25 @@ int bino(int x,int v[],int n);
 
 int main(){
 	int arr[] = {2, 4, 6, 7, 9, 29, 45, 46, 49, 50, 51};
-    printf("%d", bino(2, arr, 10)); //element,array,array's length
+    printf("%d\n", bino(2, arr, 10)); //element,array,array's length
 }
 
 int bino(int x,int v[],int n){
 	int low,high,mid ;
 	low = 0;
 	high =n-1;
-	while (low <= high) {
-		mid = (low + high) / 2;
-		if (x < v[mid]) {
+	mid = (low + high) / 2;
+	while (low < high && x != v[mid]) {
+		if (x > v[mid]) {
+			low = mid + 1;
+		}else {
 			high = mid - 1;
 		}
-		else if(x > v[mid]){
-			low = mid + 1;
-		}
-		else {
-			return mid;
-		}
+		mid = (low + high);
 	}
-	return -1;
+	if (x ==v[mid]) {
+		return mid;
+	}else {
+		return -1;
+	}
 }
