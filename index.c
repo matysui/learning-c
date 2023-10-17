d137452a3f64d9ab8fd0921aade59035ea7ab6dc
Renan Gabriel <126295397+renan8e@users.noreply.github.com>
2023-10-17 04:40:23 -0300

    switch count digits

--- index.c
+++ index.c
@@ -3,26 +3,39 @@
 int bino(int x,int v[],int n);
 
 int main(){
-	int arr[] = {2, 4, 6, 7, 9, 29, 45, 46, 49, 50, 51};
-    printf("%d\n", bino(2, arr, 10)); //element,array,array's length
-}
+	int c, i, nwhite, nother,ndigit[10];
+	nwhite = nother = 0;
 
-int bino(int x,int v[],int n){
-	int low,high,mid ;
-	low = 0;
-	high =n-1;
-	mid = (low + high) / 2;
-	while (low < high && x != v[mid]) {
-		if (x > v[mid]) {
-			low = mid + 1;
-		}else {
-			high = mid - 1;
+	for(i=0;i<10;i++){
+		ndigit[i] =0 ;
+	}
+
+	while ((c = getchar()) != EOF){
+		switch (c) {
+			case '0':case '1': case '2': case '3': case '4':
+			case '5': case '6': case '7': case '8': case '9':
+				ndigit[c-'0']++;/*Ex : 9 = 57 | (57 - 48)= 9 | ndigit[9 + 1]
+								 *a[i]++ increments the element at index i,
+								 *it doesn't increment i. And a[i++] increments i,
+								 *not the element at index i. */
+				break;
+			case ' ':
+			case '\n':
+			case '\t':
+				nwhite++;
+				break;
+			default:
+				nother++;
+				break;
+
 		}
-		mid = (low + high);
 	}
-	if (x ==v[mid]) {
-		return mid;
-	}else {
-		return -1;
+	printf("digits = ");
+	for (i=0;i<10;i++) {
+		printf("%d",ndigit[i]);
 	}
+	printf(",ws = %d, other = %d",nwhite,nother);
+
+	return 0;
+
 }
