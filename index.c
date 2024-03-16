eb5de9e687623c3dd3b09785eddd65ee6a4d5dd2
Renan Gabriel <126295397+renan8e@users.noreply.github.com>
2024-03-15 22:45:08 -0300

    squeeze-isspace-isdigit-reverse-strcat

--- index.c
+++ index.c
@@ -3,16 +3,55 @@
 int strLen( char string[] );
 int trim (char string[] );
 void getLine( char s[] );
-
+void squeeze( char s[], int n );
+int isSpace(char s);
+int isDigit(char s);
+void reversar(char v[]);
+void strCat(char s[], char t[]);
 
 int main(){
-    char s[1000] = " ";
-    getLine(s);
-    trim(s);
-    printf("%d\n",strLen(s));
+}
+
+/* strcat:  concatenate t to end of s; s must be big enough */
+void strCat(char s[], char t[]){
+    int i, j ;
+    i = j = 0;
+
+    while( /*s[i] != '\n'*/ s[i] != '\0') /* if change to '\n' pay atenton in order of logic operators.
+                                           *finde de end of s */
+        i++;
+    while((s[i++] = t[j++]) != '\0') /*copy t*/
+        ;
+}
+
+/* reverse string s in place */
+void reversar(char v[]){
+    int c, i, j;
+    for (i = 0, j = strLen(v)-1; i < j; i++, j--)
+        c = v[i], v[i] = v[j] , v[j] = c ;
 }
 
 
+int isSpace(char s){
+    if( s == ' ' || s == '\n' || s == '\t' )
+        return 1;
+    return -1;
+}
+
+int isDigit(char s) {
+    if (s >= '0' && s <= '9')
+        return 1;
+    return -1;
+}
+
+/* squeeze:  delete all c from s */
+void squeeze( char s[], int c){
+    int j,i;
+    for (i = j = 0; s[i] != '\0' ; i++)
+        if(s[i] != c)
+            s[j++] = s[i];
+    s[j] = '\0';
+}
 /* trim:  remove trailing blanks, tabs, newlines from the end of the string*/
 int trim (char s[]){
     int n;
